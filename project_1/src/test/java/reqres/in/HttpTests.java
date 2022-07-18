89f777b1c0efe527a8cb5c385a14bc05d73580a1
nastasj <78635647+nastasj@users.noreply.github.com>
2022-07-18 19:21:06 +0300

    add tests

--- project_1/src/test/java/reqres/in/HttpTests.java
+++ project_1/src/test/java/reqres/in/HttpTests.java
@@ -1,5 +1,6 @@
 package reqres.in;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -7,6 +8,7 @@ import org.apache.http.*;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpPatch;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
@@ -14,11 +16,12 @@ import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class HttpTests {
+
+    private int id;
 
     @Test
     public void testGetUser() throws IOException {
@@ -32,9 +35,29 @@ public class HttpTests {
 
     @Test
     public void testCreateUser() throws IOException {
-        User newUser = new User().withName("Nastya").withJob("QA");
+        User newUser = new User();
+        newUser.setName("Nastya");
+        newUser.setJob("QA");
         String userData = createUser(newUser);
         System.out.println(userData);
+        Assert.assertEquals(new Gson().fromJson(userData,JsonObject.class).get("name").toString()
+                .replace("\"", ""),"Nastya");
+        Assert.assertEquals(new Gson().fromJson(userData,JsonObject.class).get("job").toString()
+                .replace("\"", ""),"QA");
+
+    }
+
+    @Test
+    public void testUpdateUser() throws IOException {
+        User updatedUser = new User();
+        updatedUser.setName("Petya");
+        updatedUser.setJob("Dev");
+        String userData2 = updateUser(updatedUser);
+        System.out.println(userData2);
+        Assert.assertEquals(new Gson().fromJson(userData2,JsonObject.class).get("name").toString()
+                .replace("\"", ""),"Petya");
+        Assert.assertEquals(new Gson().fromJson(userData2,JsonObject.class).get("job").toString()
+                .replace("\"", ""),"Dev");
     }
 
     private Object getUser() throws IOException {
@@ -45,7 +68,7 @@ public class HttpTests {
 
     }
 
-    private String createUser(User newUser) throws IOException {
+    public String createUser(User newUser) throws IOException {
 //        String json = Request.Post("https://reqres.in/api/users/")
 //                .bodyForm(new BasicNameValuePair("name", newUser.getName()),
 //                            new BasicNameValuePair("job", newUser.getJob()))
@@ -54,7 +77,7 @@ public class HttpTests {
 //        HttpResponse response = client.execute( get );
 //        int code = response.getStatusLine().getStatusCode();
 //        InputStream body = response.getEntity().getContent();
-//        return j;
+//        return json;
         HttpClient httpclient = new DefaultHttpClient();
         HttpPost httppost = new HttpPost("https://reqres.in/api/users/");
         ArrayList<NameValuePair> postParameters = new ArrayList<>(2);
@@ -65,6 +88,27 @@ public class HttpTests {
         HttpEntity responseEntity = httpResponse.getEntity();
         String response = null;
         if (httpResponse.getStatusLine().getStatusCode() == 201) {
+            response = EntityUtils.toString(responseEntity);
+        }
+        int id = new Gson().fromJson(response,JsonObject.class).get("id").getAsInt();
+        System.out.println(id);
+        return response;
+    }
+
+    private String updateUser(User updatedUser) throws IOException {
+        String userData = createUser(updatedUser);
+        int id = new Gson().fromJson(userData,JsonObject.class).get("id").getAsInt();
+        System.out.println(id);
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPatch httppatch = new HttpPatch("https://reqres.in/api/users/" + id);
+        ArrayList<NameValuePair> patchParameters = new ArrayList<>(2);
+        patchParameters.add(new BasicNameValuePair("name", updatedUser.getName()));
+        patchParameters.add(new BasicNameValuePair("job", updatedUser.getJob()));
+        httppatch.setEntity(new UrlEncodedFormEntity(patchParameters));
+        HttpResponse httpResponse = httpclient.execute(httppatch);
+        HttpEntity responseEntity = httpResponse.getEntity();
+        String response = null;
+        if (httpResponse.getStatusLine().getStatusCode() == 200) {
             response = EntityUtils.toString(responseEntity);
         }
         return response;
--- project_1/src/test/java/reqres/in/User.java
+++ project_1/src/test/java/reqres/in/User.java
@@ -1,44 +1,47 @@
 package reqres.in;
 
-import java.util.Objects;
-
 public class User {
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(name, user.name) && Objects.equals(job, user.job);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, job);
-    }
 
     public String getName() {
         return name;
     }
 
-    public User withName(String name) {
+    public void setName(String name) {
         this.name = name;
-        return this;
     }
 
     public String getJob() {
         return job;
     }
 
-    public User withJob(String job) {
+    public void setJob(String job) {
         this.job = job;
-        return this;
     }
 
     private String name;
     private String job;
 
+    public int getId(int id) {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return Objects.equals(name, user.name) && Objects.equals(job, user.job);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, job);
+//    }
 
 }
